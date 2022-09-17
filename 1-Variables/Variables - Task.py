aff9a2c2e995cfea4ef0c9ba56741855091e4587
islamnazih <islamnazih00@gmail.com>
2022-09-18 00:32:25 +0200

    Tasks

---
+++ 1-Variables/Variables - Task.py
@@ -0,0 +1,30 @@
+#creat boolean variable named x
+x = True
+
+#creat an integar variable named y
+y = 5
+#creat a float variable named z
+z = 1.5
+
+#Create a string variable names s
+s = "hello"
+#Convert the int variable to float
+float(y)
+
+#Can we convert the str to int ?
+#False can not convert 
+
+#creat list of number from 1 to 5
+one = [1,2,3,4,5]
+
+#Create a tuple from 10 to 15
+two = {10,11,12,13,14,15}
+
+#Convert the list to tuple
+tuple(one)
+
+
+
+
+
+
