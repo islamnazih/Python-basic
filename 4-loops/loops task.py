0e97607dc395db6b4ed280b2eb5b911ff9e0c245
islamnazih <islamnazih00@gmail.com>
2022-09-25 21:02:47 +0200

    Loops

---
+++ 4-loops/loops task.py
@@ -0,0 +1,4 @@
+for y in range (1,11):
+    for x in range(1,13):
+        print(f"{x} X {y} = {x*y}")
+    print("---------------------")
---
+++ 4-loops/loops.py
@@ -0,0 +1,33 @@
+"""
+friend = ["is","ali","mo","mh","ZZ","bb",10,20,"moa","all",5]
+
+a = 0
+while a < len(friend) :
+    print(f"# {str(a + 1).zfill(6)} {friend[a]}")
+    a += 1
+else :
+    print("Finish")
+"""
+#_________________________________
+"""
+favsite = []
+
+maxsite = 5
+
+while maxsite > 0:
+    web = input("add website without https://")
+    favsite.append(f"https://{web.strip().lower()}")
+    maxsite -= 1
+    print(f"website added {maxsite} places left")
+    print(favsite)
+else:
+    print("full")
+
+for i in favsite:
+    print(favsite)
+"""
+
+
+
+
+
